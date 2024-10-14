37ad11bcc28545d7b77c16477b92c88b34741f6a
JorgeNeder97 <jorge.neder97@gmail.com>
2024-10-14 18:29:56 -0300

    More changes

--- frontend/src/ProtectedRoutes.tsx
+++ frontend/src/ProtectedRoutes.tsx
@@ -1,17 +1,21 @@
 import { Navigate, Outlet } from "react-router-dom";
 // import Cookies from 'js-cookie';
 import { verifyTokenRequest } from "#services/userServices.ts";
+import { useState } from "react";
 
 export const ProtectedRoutes = () => {
 
+    const [auth, setAuth] = useState<boolean>(false);
     // const token = Cookies.get('token');
     const verify = async () => {
         const verificado = await verifyTokenRequest();
-
-        if(verificado.status === 200) return <Outlet />
-        else return <Navigate to="/login" replace />;
+        if(verificado.status === 200) setAuth(true);
+        return
     }
-
+    
     verify();
 
+    if(!auth) return <Navigate to="/login" />
+    
+    return <Outlet />
 };
