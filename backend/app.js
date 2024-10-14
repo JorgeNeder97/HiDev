95326782d71382307a35c687cd7d04caefd3e83a
JorgeNeder97 <jorge.neder97@gmail.com>
2024-10-14 12:22:04 -0300

    Cambiando origin de CORS

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new SocketServer(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://hidev-client.onrender.com/",
         methods: ["GET", "POST"],
         credentials: true,
     }
