6207cf0ee854231b792293813325d4ba12c2ad1d
JorgeNeder97 <jorge.neder97@gmail.com>
2024-10-14 16:53:47 -0300

    Probando configuraciones CORS

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import http from 'http';
-import { Server as SocketServer } from 'socket.io';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-import indexRouter from './routes/index.js';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import http from "http";
+import { Server as SocketServer } from "socket.io";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import indexRouter from "./routes/index.js";
+import cors from "cors";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -14,40 +14,42 @@ const server = http.createServer(app);
 const io = new SocketServer(server, {
     cors: {
         origin: process.env.CORS_URL,
-        methods: ["GET", "POST"],
+        methods: ["GET", "POST", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
-    }
+    },
 });
 
 // Escucha por si llega un evento connection de algun cliente y si llega (si hay una conexion) ejecuta el codigo
-io.on('connection', socket => {
-    console.log('Client connected')
+io.on("connection", (socket) => {
+    console.log("Client connected");
 
     // Recibe un evento message con datos enviados por un cliente (recibe el mensaje)
-    socket.on('message', (data) => {
+    socket.on("message", (data) => {
         // Reenvia un evento message con datos (envia el mensaje) a todos los clientes conectados menos a al que envió el mensaje al servidor
-        socket.broadcast.emit('message', {
+        socket.broadcast.emit("message", {
             body: data.body,
             hour: data.hour,
             from: data.from,
         });
     });
-})
+});
 
 server.listen(3000);
-console.log('Server on port ', 3000);
+console.log("Server on port ", 3000);
 
-app.use(cors({
-    origin: process.env.CORS_URL, 
-    methods: ["GET", "POST"],
-    credentials: true // Habilitar el uso de cookies
-}));
+app.use(
+    cors({
+        origin: process.env.CORS_URL,
+        methods: ["GET", "POST", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true, // Habilitar el uso de cookies
+    })
+);
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-
-
+app.use("/", indexRouter);
--- backend/controllers/indexController.js
+++ backend/controllers/indexController.js
@@ -1,20 +1,27 @@
-import pkg from 'jsonwebtoken';
+import pkg from "jsonwebtoken";
 const { verify } = pkg;
-import jwt from 'jsonwebtoken';
-import createAccestoken from '../modules/jwt.js';
+import jwt from "jsonwebtoken";
+import createAccestoken from "../modules/jwt.js";
 
 const indexController = {
     sayHi: (req, res) => {
-        res.send('HiDev! Server')
-    }, 
+        res.send("HiDev! Server");
+    },
     login: async (req, res) => {
-        const {usuario, contraseña} = req.body;
-        if(contraseña !== process.env.PASSWORD) return res.status(400).json({ message: "Contraseña inválida" });
-        else if(contraseña === process.env.PASSWORD) {
+        const { usuario, contraseña } = req.body;
+        if (contraseña !== process.env.PASSWORD)
+            return res.status(400).json({ message: "Contraseña inválida" });
+        else if (contraseña === process.env.PASSWORD) {
             const token = await createAccestoken({
-                nombre: usuario
+                nombre: usuario,
             });
-            res.cookie("token", token);
+            res.cookie("token", token, {
+                secure: true,
+                sameSite: "Strict",
+                maxAge: 24 * 60 * 60 * 1000,
+                domain: process.env.CORS_URL,
+                path: "/",
+            });
             res.json({
                 nombre: usuario,
                 token,
@@ -23,21 +30,21 @@ const indexController = {
     },
     verifyToken: (req, res) => {
         const token = req.cookies.token;
-        if(!token) return res.status(401).json({ message: 'Unauthorized - No Token' });
+        if (!token)
+            return res.status(401).json({ message: "Unauthorized - No Token" });
 
         jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
             if (err) {
-              return res.status(403).send('Token inválido.');
+                return res.status(403).send("Token inválido.");
             }
-            return res.status(200).json({ message: 'Authorized!' });
+            return res.status(200).json({ message: "Authorized!" });
         });
     },
     logout: (req, res) => {
         res.cookie("token", "", { expires: new Date(0) });
         res.cookie("nombre", "", { expires: new Date(0) });
         return res.sendStatus(200);
-    }
-
-}
+    },
+};
 
 export default indexController;
